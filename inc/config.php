b10741a1a2f64a5c32465710c38a35573ec849a4
Ronaldo Rocha <ronaldocuca@gmail.com>
2016-09-18 14:39:28 -0300

    conexao

--- inc/config.php
+++ inc/config.php
@@ -10,6 +10,8 @@ function conecta()
 	$banco = "dd023q6nm7ia09";
 	$port = "5432";
 
-	$conexao = pg_connect("host={$host},port={$port},dbname={$banco},user={$usuario},password={$senha}");
+	$conexao = pg_connect("host={$host} port={$port} dbname={$banco} user={$usuario} password={$senha}");
 	return $conexao;
 }
+
+conecta();
