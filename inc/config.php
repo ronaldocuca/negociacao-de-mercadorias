a6fcae069da826ada16d6bb3afe738728af6cd0d
Ronaldo Rocha <ronaldocuca@gmail.com>
2016-09-18 14:29:08 -0300

    install

--- inc/config.php
+++ inc/config.php
@@ -8,7 +8,7 @@ function conecta()
 	$usuario = "gvoeagrihtmjde";
 	$senha = "lD3eR2BJHrs76lQz5OefL8LW1r";
 	$banco = "dd023q6nm7ia09";
-	$port = 5432;
+	$port = "5432";
 
 	$conexao = pg_connect("host={$host},port={$port},dbname={$banco},user={$usuario},password={$senha}");
 	return $conexao;
