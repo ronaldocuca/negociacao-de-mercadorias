65db9c2fbea5395b6b8f78d583117ad31ce72252
Ronaldo Rocha <ronaldocuca@gmail.com>
2016-09-18 13:38:38 -0300

    mysql para postgres

--- inc/config.php
+++ inc/config.php
@@ -4,11 +4,12 @@
 ---------------------------------------------------------------------------------------------------*/
 function conecta()
 {
-	$host = "localhost";
-	$usuario = "root";
-	$senha = "root";
-	$banco = "bd_teste";
+	$host = "ec2-54-235-195-226.compute-1.amazonaws.com";
+	$usuario = "gvoeagrihtmjde";
+	$senha = "lD3eR2BJHrs76lQz5OefL8LW1r";
+	$banco = "dd023q6nm7ia09";
+	$port = 5432;
 
-	$conexao = mysqli_connect($host,$usuario,$senha,$banco);
+	$conexao = pg_connect("host={$host},port={$port},dbname={$banco},user={$usuario},password={$senha}");
 	return $conexao;
 }
