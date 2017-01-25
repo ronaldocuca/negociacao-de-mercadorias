1b418a9bd18a49b3b2fa46fbdd0e3fcf3fa407f7
Ronaldo Rocha <ronaldocuca@gmail.com>
2017-01-25 15:50:33 -0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 			</footer>
 			<!-- include JS -->
 			<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-	        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+	        <!--<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>-->
 	        <script src="js/jquery-validation-1.15.0/jquery.validate.js"></script>
 	        <script src="js/bootstrap.js" type="text/javascript"></script>
 	        <script src="js/script.js" type="text/javascript"></script>
